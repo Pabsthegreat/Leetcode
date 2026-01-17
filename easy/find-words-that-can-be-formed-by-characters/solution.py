0b30a6166273ddd4fc4835a009c1fa6c1a3d4f39
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-17 18:06:24 +0530

    Sync solution: Find Words That Can Be Formed by Characters

---
+++ easy/find-words-that-can-be-formed-by-characters/solution.py
@@ -0,0 +1,56 @@
+"""
+[Description]
+Find Words That Can Be Formed by Characters
+https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
+
+You are given an array of strings words and a string chars.
+
+A string is good if it can be formed by characters from chars (each character can only be used once for each word in words).
+
+Return the sum of lengths of all good strings in words.
+
+ 
+Example 1:
+
+Input: words = ["cat","bt","hat","tree"], chars = "atach"
+Output: 6
+Explanation: The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+
+Example 2:
+
+Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+Output: 10
+Explanation: The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+
+ 
+Constraints:
+
+  1 <= words.length <= 1000
+  1 <= words[i].length, chars.length <= 100
+  words[i] and chars consist of lowercase English letters.
+
+[Metadata]
+- Difficulty: Easy
+- Topics: Array, Hash Table, String, Counting
+- Slug: find-words-that-can-be-formed-by-characters
+"""
+
+// [Solution]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ind = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in ind:
+                return [ind[complement], i] 
+            
+            ind[nums[i]] = i
+        
+        return None
