b75007b4491efd14c1731035cd20b5f3ab6860e9
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-17 18:00:57 +0530

    Sync solution: Sqrt(x)

---
+++ easy/sqrtx/solution.py
@@ -0,0 +1,54 @@
+"""
+[Description]
+Sqrt(x)
+https://leetcode.com/problems/sqrtx/
+
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+  For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+ 
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+
+ 
+Constraints:
+
+  0 <= x <= 231 - 1
+
+[Metadata]
+- Difficulty: Easy
+- Topics: Math, Binary Search
+- Slug: sqrtx
+"""
+
+// [Solution]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ind = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in ind:
+                return [ind[complement], i] 
+            
+            ind[nums[i]] = i
+        
+        return None
