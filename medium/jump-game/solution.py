4ac38936db2456dfe421bcd1183b151229721088
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-17 18:00:21 +0530

    Sync solution: Jump Game

---
+++ medium/jump-game/solution.py
@@ -0,0 +1,53 @@
+"""
+[Description]
+Jump Game
+https://leetcode.com/problems/jump-game/
+
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+ 
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+ 
+Constraints:
+
+  1 <= nums.length <= 104
+  0 <= nums[i] <= 105
+
+[Metadata]
+- Difficulty: Medium
+- Topics: Array, Dynamic Programming, Greedy
+- Slug: jump-game
+"""
+
+// [Solution]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ind = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in ind:
+                return [ind[complement], i] 
+            
+            ind[nums[i]] = i
+        
+        return None
