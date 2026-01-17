1058c64dc136fec1ae2046b1c49e2711610a3832
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-17 17:59:15 +0530

    Sync solution: Longest Common Prefix

---
+++ easy/longest-common-prefix/solution.py
@@ -0,0 +1,53 @@
+"""
+[Description]
+Longest Common Prefix
+https://leetcode.com/problems/longest-common-prefix/
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+ 
+Constraints:
+
+  1 <= strs.length <= 200
+  0 <= strs[i].length <= 200
+  strs[i] consists of only lowercase English letters if it is non-empty.
+
+[Metadata]
+- Difficulty: Easy
+- Topics: Array, String, Trie
+- Slug: longest-common-prefix
+"""
+
+// [Solution]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ind = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in ind:
+                return [ind[complement], i] 
+            
+            ind[nums[i]] = i
+        
+        return None
