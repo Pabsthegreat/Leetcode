4f46ad9ed62252671de171e2afd596586618c2d1
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-17 18:10:14 +0530

    Sync solution: Maximum Odd Binary Number

---
+++ easy/maximum-odd-binary-number/solution.py
@@ -0,0 +1,58 @@
+"""
+[Description]
+Maximum Odd Binary Number
+https://leetcode.com/problems/maximum-odd-binary-number/
+
+You are given a binary string s that contains at least one '1'.
+
+You have to rearrange the bits in such a way that the resulting binary number is the maximum odd binary number that can be created from this combination.
+
+Return a string representing the maximum odd binary number that can be created from the given combination.
+
+Note that the resulting string can have leading zeros.
+
+ 
+Example 1:
+
+Input: s = "010"
+Output: "001"
+Explanation: Because there is just one '1', it must be in the last position. So the answer is "001".
+
+Example 2:
+
+Input: s = "0101"
+Output: "1001"
+Explanation: One of the '1's must be in the last position. The maximum number that can be made with the remaining digits is "100". So the answer is "1001".
+
+ 
+Constraints:
+
+  1 <= s.length <= 100
+  s consists only of '0' and '1'.
+  s contains at least one '1'.
+
+[Metadata]
+- Difficulty: Easy
+- Topics: Math, String, Greedy
+- Slug: maximum-odd-binary-number
+"""
+
+// [Solution]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ind = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in ind:
+                return [ind[complement], i] 
+            
+            ind[nums[i]] = i
+        
+        return None
