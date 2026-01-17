79a26bd7d4ea597aabdc5d208e1cf9adc351156a
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-17 17:59:32 +0530

    Sync solution: Merge Two Sorted Lists

---
+++ easy/merge-two-sorted-lists/solution.py
@@ -0,0 +1,59 @@
+"""
+[Description]
+Merge Two Sorted Lists
+https://leetcode.com/problems/merge-two-sorted-lists/
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+Example 1:
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+ 
+Constraints:
+
+  The number of nodes in both lists is in the range [0, 50].
+  -100 <= Node.val <= 100
+  Both list1 and list2 are sorted in non-decreasing order.
+
+[Metadata]
+- Difficulty: Easy
+- Topics: Linked List, Recursion
+- Slug: merge-two-sorted-lists
+"""
+
+// [Solution]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ind = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in ind:
+                return [ind[complement], i] 
+            
+            ind[nums[i]] = i
+        
+        return None
