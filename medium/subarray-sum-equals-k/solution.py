7230611f56bd811aafccb46d0d5977f54611d49c
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-17 18:05:33 +0530

    Sync solution: Subarray Sum Equals K

---
+++ medium/subarray-sum-equals-k/solution.py
@@ -0,0 +1,49 @@
+"""
+[Description]
+Subarray Sum Equals K
+https://leetcode.com/problems/subarray-sum-equals-k/
+
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+Example 1:
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+Input: nums = [1,2,3], k = 3
+Output: 2
+
+ 
+Constraints:
+
+  1 <= nums.length <= 2 * 104
+  -1000 <= nums[i] <= 1000
+  -107 <= k <= 107
+
+[Metadata]
+- Difficulty: Medium
+- Topics: Array, Hash Table, Prefix Sum
+- Slug: subarray-sum-equals-k
+"""
+
+// [Solution]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ind = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in ind:
+                return [ind[complement], i] 
+            
+            ind[nums[i]] = i
+        
+        return None
