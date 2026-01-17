a3027cf2944583969cd27368f75857173fd966ac
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-17 18:09:47 +0530

    Sync solution: Maximum Sum of Distinct Subarrays With Length K

---
+++ medium/maximum-sum-of-distinct-subarrays-with-length-k/solution.py
@@ -0,0 +1,66 @@
+"""
+[Description]
+Maximum Sum of Distinct Subarrays With Length K
+https://leetcode.com/problems/maximum-sum-of-distinct-subarrays-with-length-k/
+
+You are given an integer array nums and an integer k. Find the maximum subarray sum of all the subarrays of nums that meet the following conditions:
+
+  The length of the subarray is k, and
+  All the elements of the subarray are distinct.
+
+Return the maximum subarray sum of all the subarrays that meet the conditions. If no subarray meets the conditions, return 0.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+Example 1:
+
+Input: nums = [1,5,4,2,9,9,9], k = 3
+Output: 15
+Explanation: The subarrays of nums with length 3 are:
+- [1,5,4] which meets the requirements and has a sum of 10.
+- [5,4,2] which meets the requirements and has a sum of 11.
+- [4,2,9] which meets the requirements and has a sum of 15.
+- [2,9,9] which does not meet the requirements because the element 9 is repeated.
+- [9,9,9] which does not meet the requirements because the element 9 is repeated.
+We return 15 because it is the maximum subarray sum of all the subarrays that meet the conditions
+
+Example 2:
+
+Input: nums = [4,4,4], k = 3
+Output: 0
+Explanation: The subarrays of nums with length 3 are:
+- [4,4,4] which does not meet the requirements because the element 4 is repeated.
+We return 0 because no subarrays meet the conditions.
+
+ 
+Constraints:
+
+  1 <= k <= nums.length <= 105
+  1 <= nums[i] <= 105
+
+[Metadata]
+- Difficulty: Medium
+- Topics: Array, Hash Table, Sliding Window
+- Slug: maximum-sum-of-distinct-subarrays-with-length-k
+"""
+
+// [Solution]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ind = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in ind:
+                return [ind[complement], i] 
+            
+            ind[nums[i]] = i
+        
+        return None
