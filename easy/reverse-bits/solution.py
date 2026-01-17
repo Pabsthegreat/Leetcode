3d0cf8fcf5a8e9dba02e836872cfacac458e88ee
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-17 18:02:59 +0530

    Sync solution: Reverse Bits

---
+++ easy/reverse-bits/solution.py
@@ -0,0 +1,88 @@
+"""
+[Description]
+Reverse Bits
+https://leetcode.com/problems/reverse-bits/
+
+Reverse bits of a given 32 bits signed integer.
+
+ 
+Example 1:
+
+Input: n = 43261596
+
+Output: 964176192
+
+Explanation:
+
+  
+    
+      Integer
+      Binary
+    
+    
+      43261596
+      00000010100101000001111010011100
+    
+    
+      964176192
+      00111001011110000010100101000000
+    
+  
+
+Example 2:
+
+Input: n = 2147483644
+
+Output: 1073741822
+
+Explanation:
+
+  
+    
+      Integer
+      Binary
+    
+    
+      2147483644
+      01111111111111111111111111111100
+    
+    
+      1073741822
+      00111111111111111111111111111110
+    
+  
+
+ 
+Constraints:
+
+  0 <= n <= 231 - 2
+  n is even.
+
+ 
+Follow up: If this function is called many times, how would you optimize it?
+
+[Metadata]
+- Difficulty: Easy
+- Topics: Divide and Conquer, Bit Manipulation
+- Slug: reverse-bits
+"""
+
+// [Solution]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ind = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in ind:
+                return [ind[complement], i] 
+            
+            ind[nums[i]] = i
+        
+        return None
