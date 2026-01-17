f8ae554a366383933a8e1f1ccac0ae01a621464b
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-17 18:04:47 +0530

    Sync solution: Ransom Note

---
+++ easy/ransom-note/solution.py
@@ -0,0 +1,51 @@
+"""
+[Description]
+Ransom Note
+https://leetcode.com/problems/ransom-note/
+
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+ 
+Example 1:
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+
+ 
+Constraints:
+
+  1 <= ransomNote.length, magazine.length <= 105
+  ransomNote and magazine consist of lowercase English letters.
+
+[Metadata]
+- Difficulty: Easy
+- Topics: Hash Table, String, Counting
+- Slug: ransom-note
+"""
+
+// [Solution]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ind = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in ind:
+                return [ind[complement], i] 
+            
+            ind[nums[i]] = i
+        
+        return None
