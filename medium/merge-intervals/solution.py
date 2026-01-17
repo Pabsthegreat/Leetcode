2ab8e2f132dff162d9a81ca9454e1aa430e7e630
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-17 18:00:28 +0530

    Sync solution: Merge Intervals

---
+++ medium/merge-intervals/solution.py
@@ -0,0 +1,58 @@
+"""
+[Description]
+Merge Intervals
+https://leetcode.com/problems/merge-intervals/
+
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+ 
+Example 1:
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+Example 3:
+
+Input: intervals = [[4,7],[1,4]]
+Output: [[1,7]]
+Explanation: Intervals [1,4] and [4,7] are considered overlapping.
+
+ 
+Constraints:
+
+  1 <= intervals.length <= 104
+  intervals[i].length == 2
+  0 <= starti <= endi <= 104
+
+[Metadata]
+- Difficulty: Medium
+- Topics: Array, Sorting
+- Slug: merge-intervals
+"""
+
+// [Solution]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ind = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in ind:
+                return [ind[complement], i] 
+            
+            ind[nums[i]] = i
+        
+        return None
