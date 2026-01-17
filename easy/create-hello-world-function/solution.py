29390f08ae0eddbc271bd5ec3ee34e56465db1ea
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-17 18:10:08 +0530

    Sync solution: Create Hello World Function

---
+++ easy/create-hello-world-function/solution.py
@@ -0,0 +1,57 @@
+"""
+[Description]
+Create Hello World Function
+https://leetcode.com/problems/create-hello-world-function/
+
+Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+ 
+Example 1:
+
+Input: args = []
+Output: "Hello World"
+Explanation:
+const f = createHelloWorld();
+f(); // "Hello World"
+
+The function returned by createHelloWorld should always return "Hello World".
+
+Example 2:
+
+Input: args = [{},null,42]
+Output: "Hello World"
+Explanation:
+const f = createHelloWorld();
+f({}, null, 42); // "Hello World"
+
+Any arguments could be passed to the function but it should still always return "Hello World".
+
+ 
+Constraints:
+
+  0 <= args.length <= 10
+
+[Metadata]
+- Difficulty: Easy
+- Topics: 
+- Slug: create-hello-world-function
+"""
+
+// [Solution]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ind = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in ind:
+                return [ind[complement], i] 
+            
+            ind[nums[i]] = i
+        
+        return None
