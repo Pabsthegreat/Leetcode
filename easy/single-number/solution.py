550e0f66590b294dfdb49d13f3509640e75de5e0
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-17 18:02:09 +0530

    Sync solution: Single Number

---
+++ easy/single-number/solution.py
@@ -0,0 +1,60 @@
+"""
+[Description]
+Single Number
+https://leetcode.com/problems/single-number/
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+Example 1:
+
+Input: nums = [2,2,1]
+
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+
+Output: 1
+
+ 
+Constraints:
+
+  1 <= nums.length <= 3 * 104
+  -3 * 104 <= nums[i] <= 3 * 104
+  Each element in the array appears twice except for one element which appears only once.
+
+[Metadata]
+- Difficulty: Easy
+- Topics: Array, Bit Manipulation
+- Slug: single-number
+"""
+
+// [Solution]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ind = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in ind:
+                return [ind[complement], i] 
+            
+            ind[nums[i]] = i
+        
+        return None
