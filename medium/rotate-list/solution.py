0db470ad1cbf3eb9246c005ebb54303e4cd3af88
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-17 18:00:45 +0530

    Sync solution: Rotate List

---
+++ medium/rotate-list/solution.py
@@ -0,0 +1,50 @@
+"""
+[Description]
+Rotate List
+https://leetcode.com/problems/rotate-list/
+
+Given the head of a linked list, rotate the list to the right by k places.
+
+ 
+Example 1:
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+
+Example 2:
+
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+
+ 
+Constraints:
+
+  The number of nodes in the list is in the range [0, 500].
+  -100 <= Node.val <= 100
+  0 <= k <= 2 * 109
+
+[Metadata]
+- Difficulty: Medium
+- Topics: Linked List, Two Pointers
+- Slug: rotate-list
+"""
+
+// [Solution]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ind = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            
+            if complement in ind:
+                return [ind[complement], i] 
+            
+            ind[nums[i]] = i
+        
+        return None
