44b5a074b68c6014fd8fc8e68998dae6c450b5f8
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-19 19:23:08 +0530

    Sync solution: Concatenation of Array

--- easy/concatenation-of-array/solution.py
+++ easy/concatenation-of-array/solution.py
@@ -40,21 +40,6 @@ Constraints:
 """
 
 // [Solution]
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        ind = {}
-        
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            
-            if complement in ind:
-                return [ind[complement], i] 
-            
-            ind[nums[i]] = i
-        
-        return None
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
