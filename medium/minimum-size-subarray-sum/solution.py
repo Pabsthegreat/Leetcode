4895696386bc8252a918b258844cae455896531f
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-19 19:19:51 +0530

    Sync solution: Minimum Size Subarray Sum

--- medium/minimum-size-subarray-sum/solution.py
+++ medium/minimum-size-subarray-sum/solution.py
@@ -40,20 +40,16 @@ Follow up: If you have figured out the O(n) solution, try coding another solutio
 
 // [Solution]
 class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        ind = {}
-        
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            
-            if complement in ind:
-                return [ind[complement], i] 
-            
-            ind[nums[i]] = i
-        
-        return None
+    def minSubArrayLen(self, target, nums):
+        left = 0
+        curr_sum = 0
+        min_len = float('inf')
+
+        for right in range(len(nums)):
+            curr_sum += nums[right]
+            while curr_sum >= target:
+                min_len = min(min_len, right - left + 1)
+                curr_sum -= nums[left]
+                left += 1
+
+        return 0 if min_len == float('inf') else min_len
