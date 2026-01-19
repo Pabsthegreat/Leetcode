47469311d12eccad049c8ef6c9b16d7c49bef303
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-19 19:21:21 +0530

    Sync solution: Rotate String

--- easy/rotate-string/solution.py
+++ easy/rotate-string/solution.py
@@ -30,21 +30,15 @@ Constraints:
 """
 
 // [Solution]
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        ind = {}
-        
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            
-            if complement in ind:
-                return [ind[complement], i] 
-            
-            ind[nums[i]] = i
-        
-        return None
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if s==goal:
+            return True
+        shift = len(s)
+        while shift>0 and s != goal:
+            s = s[1:]+s[0]
+            shift-=1
+        if shift>0:
+            return True
+
+        return False
