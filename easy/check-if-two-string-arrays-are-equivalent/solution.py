0a82b4f757fb8a6703a4b1d9dfe516911581d11b
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-19 19:22:31 +0530

    Sync solution: Check If Two String Arrays are Equivalent

--- easy/check-if-two-string-arrays-are-equivalent/solution.py
+++ easy/check-if-two-string-arrays-are-equivalent/solution.py
@@ -42,21 +42,6 @@ Constraints:
 """
 
 // [Solution]
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        ind = {}
-        
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            
-            if complement in ind:
-                return [ind[complement], i] 
-            
-            ind[nums[i]] = i
-        
-        return None
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        return "".join(word1)=="".join(word2)
