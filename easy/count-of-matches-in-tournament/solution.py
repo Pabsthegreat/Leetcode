c879f8d9f21c343c59d9ccdb4d1d1ddd8f66da7c
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-19 19:22:38 +0530

    Sync solution: Count of Matches in Tournament

--- easy/count-of-matches-in-tournament/solution.py
+++ easy/count-of-matches-in-tournament/solution.py
@@ -44,21 +44,6 @@ Constraints:
 """
 
 // [Solution]
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        ind = {}
-        
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            
-            if complement in ind:
-                return [ind[complement], i] 
-            
-            ind[nums[i]] = i
-        
-        return None
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        return n-1
