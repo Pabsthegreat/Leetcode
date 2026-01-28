70e1032d67e73cd95305cd040ed8242c1eb7ffe5
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-01-28 11:55:15 +0530

    Sync solution: Valid Sudoku

---
+++ medium/valid-sudoku/solution.py
@@ -0,0 +1,90 @@
+"""
+[Description]
+Valid Sudoku
+https://leetcode.com/problems/valid-sudoku/description/
+
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+  Each row must contain the digits 1-9 without repetition.
+  Each column must contain the digits 1-9 without repetition.
+  Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+Note:
+
+  A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+  Only the filled cells need to be validated according to the mentioned rules.
+
+ 
+Example 1:
+
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+ 
+Constraints:
+
+  board.length == 9
+  board[i].length == 9
+  board[i][j] is a digit 1-9 or '.'.
+
+[Metadata]
+- Difficulty: Medium
+- Topics: Array, Hash Table, Matrix
+- Slug: valid-sudoku
+"""
+
+// [Solution]
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        grid = {i:set() for i in range(9)}
+        column = {i:set() for i in range(9)}
+        row = {i:set() for i in range(9)}
+
+        for i in range(9):
+            for j in range(9):
+                g = (i//3)*3+j//3
+                if board[i][j] == ".":
+                    continue
+                if board[i][j] in grid[g]:
+                    return False
+                else:
+                    grid[g].add(board[i][j])
+                
+                if board[i][j] in column[j]:
+                    return False
+                else:
+                    column[j].add(board[i][j])
+
+                if board[i][j] in row[i]:
+                    return False
+                else:
+                    row[i].add(board[i][j])
+        return True
