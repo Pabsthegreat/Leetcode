b588ab7d7506c97d14f0e0279f28ca8a282c96f0
Adarsh Arun Hoskere <94716164+Pabsthegreat@users.noreply.github.com>
2026-06-22 09:40:42 +0530

    Sync solution: Substring with Concatenation of All Words

---
+++ hard/substring-with-concatenation-of-all-words/solution.py
@@ -0,0 +1,80 @@
+"""
+[Description]
+Substring with Concatenation of All Words
+https://leetcode.com/problems/substring-with-concatenation-of-all-words/
+
+You are given a string s and an array of strings words. All the strings of words are of the same length.
+
+A concatenated string is a string that exactly contains all the strings of any permutation of words concatenated.
+
+  For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are all concatenated strings. "acdbef" is not a concatenated string because it is not the concatenation of any permutation of words.
+
+Return an array of the starting indices of all the concatenated substrings in s. You can return the answer in any order.
+
+ 
+Example 1:
+
+Input: s = "barfoothefoobarman", words = ["foo","bar"]
+
+Output: [0,9]
+
+Explanation:
+
+The substring starting at 0 is "barfoo". It is the concatenation of ["bar","foo"] which is a permutation of words.
+The substring starting at 9 is "foobar". It is the concatenation of ["foo","bar"] which is a permutation of words.
+
+Example 2:
+
+Input: s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+
+Output: []
+
+Explanation:
+
+There is no concatenated substring.
+
+Example 3:
+
+Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+
+Output: [6,9,12]
+
+Explanation:
+
+The substring starting at 6 is "foobarthe". It is the concatenation of ["foo","bar","the"].
+The substring starting at 9 is "barthefoo". It is the concatenation of ["bar","the","foo"].
+The substring starting at 12 is "thefoobar". It is the concatenation of ["the","foo","bar"].
+
+ 
+Constraints:
+
+  1 <= s.length <= 104
+  1 <= words.length <= 5000
+  1 <= words[i].length <= 30
+  s and words[i] consist of lowercase English letters.
+
+[Metadata]
+- Difficulty: Hard
+- Topics: Hash Table, String, Sliding Window
+- Slug: substring-with-concatenation-of-all-words
+"""
+
+// [Solution]
+from collections import Counter
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        d = Counter(words)
+        word_len = len(words[0])
+        l = len(words) * word_len
+        result = []
+        for i in range(0, len(s) - l + 1):
+            window = s[i:i + l]
+            chunks = [window[j:j + word_len] for j in range(0, l, word_len)]
+            if Counter(chunks) == d:
+                result.append(i)
+        return result
